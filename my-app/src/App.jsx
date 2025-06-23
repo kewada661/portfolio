dd204d76bc56a0302ca4445ff3cb1ebda99e0ebe
kwada661 <kwada@usc.edu>
2025-06-23 02:26:49 -0700

    clean up

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -14,12 +14,6 @@ function App() {
     <>
       <div>
         <h1>kendall wada</h1>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
         <div className="thumbnailContainer">
           <img src={gorilla} className="thumbnail portraiture" alt="Portraiture Thumbnail" />
         </div>
@@ -32,17 +26,6 @@ function App() {
         <div className="thumbnailContainer">
           <img src={covers} className="thumbnail newsletters" alt="Newsletters Thumbnail" />
         </div>      </div>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR. Hello World!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
